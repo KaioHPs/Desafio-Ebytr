6513dac843eea93deb10defe2a5d3c0ba3f15a16
KaioHPs <kaiohpsalternativo@gmail.com>
2021-11-05 01:10:53 -0300

    Listagem de tarefas e botao para deletar tarefa

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,13 +12,44 @@ function App() {
     .then((r) => r.json())
     .then((r) => setTasks(r));
 
+  const deleteTask = async (id) => await fetch(`http://localhost:3001/tasks/${id}`, {
+    method: 'DELETE',
+  })
+    .then((r) => r.json())
+    .then((r) => console.log(r))
+    .then(() => getTask());
+
   return (
     <div className="App">
-      <button onClick={getTask}>Exibir todas as tarefas</button>
-      <button onClick={() => setTasks([])}>Limpar lista de tarefas</button>
+      <div className='btn-container'>
+        <button
+          onClick={getTask}
+          className='btnGet'
+        >
+          Exibir todas as tarefas
+        </button>
+        <button
+          onClick={() => setTasks([])}
+          className='btnClean'
+        >
+          Limpar lista de tarefas
+        </button>
+      </div>
       <div>
         <ul>
-          {tasks.map((t, i) => <li key={i}> {t.task} - {t.taskStatus} </li>)}
+          {tasks.map((t, i) => {
+            return (
+              <li key={i} className='task-li'>
+                <span>{t.task} - {t.taskStatus}</span>
+                <button
+                  className='btnDelete'
+                  onClick={() => deleteTask(t._id)}
+                >
+                  Excluir Tarefa
+                </button>
+              </li>
+            );
+          })}
           { console.log(tasks) }
         </ul>
       </div>
@@ -27,3 +58,4 @@ function App() {
 }
 
 export default App;
+// git commit -m 'Listagem de tarefas e botao para deletar tarefa'
